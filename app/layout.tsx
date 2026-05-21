4240ef6ff307164b1165fb8d3f18afa6349e5cd0
Dhiraj Parihar <dhirajparihar2001@gmail.com>
2026-05-21 19:22:03 +0530

    added ad script gooogle

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5507752351572129"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className="min-h-full flex flex-col">
         <GlobalHeader />
         {children}
